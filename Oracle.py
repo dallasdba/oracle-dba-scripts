6321c7579a338203bb5b73028ff7f55364e207b0
dallasdba <datahead@twc.com>
2019-07-12 12:11:09 -0500

    Added the LoadOratab() fucntion.

--- Oracle.py
+++ Oracle.py
@@ -5,6 +5,7 @@
 #               code here in order to simplify managing changes to duplicate code across dozens  #
 #               of scripts.                                                                      #
 #                                                                                                #
+#  Functions:   LoadOratab(Oratab='')                                                            #
 ##################################################################################################
 
 # --------------------------------------
@@ -70,6 +71,75 @@ signal(SIGPIPE, SIG_DFL)
 PyMaxVer = 3.4
 PyMinVer = 2.4
 
-# -------------------------------------------------
+# ------------------------------------------------
 # ---- Function and Class Definitions ------------
-# -------------------------------------------------
+# ------------------------------------------------
+# ---------------------------------------------------------------------------
+# Def : LoadOratab()
+# Desc: Parses the oratab file and returns a dictionary structure of:
+#        {'dbm'      : '/u01/app/oracle/product/11.2.0.3/dbhome_1',
+#         'biuat'    : '/u01/app/oracle/product/11.2.0.3/dbhome_1',
+#         ...
+#        }
+#       Note** the start/stop flag is parsed but not saved.
+#       If the fully qualified oratab file name is passed in it is prepended
+#       to a list of standard locations (/etc/oratab, /var/opt/oracle/oratab)
+#       This list of oratab locations are then searched in order. The first
+#       one to be successfully opened will be used.
+# Args: Oratab (optional, defaults to '')
+# Retn: OratabDict (dictionary object)
+# ---------------------------------------------------------------------------
+def LoadOratab(Oratab=''):
+  OraSid     = ''
+  OraHome    = ''
+  OraFlag    = ''
+  OratabDict = {}
+  OratabList = []
+  OratabLoc  = ['/etc/oratab','/var/opt/oracle/oratab']
+  f          = None
+  
+  # If an _ORATAB_ environment variable is set...
+  if '_ORATAB_' in environ:
+    if (not (environ['_ORATAB_'] in OratabLoc)):
+      OratabLoc.insert(0, environ['_ORATAB_'])
+
+  # If an oratab file name was passed in...
+  if (Oratab != ''):
+    if (not (Oratab in OratabLoc)):
+      OratabLoc.insert(0, Oratab)
+
+  # Load the first readable file in OratabLoc and then break.
+  for Oratab in OratabLoc:
+    if (isfile(Oratab)):
+      try:
+        f = open(Oratab)
+        break
+      except:
+        print('\n%s' % traceback.format_exc())
+        print('\nCannot open oratab file: ' + Oratab + ' for read.')
+        return {}
+
+  # Parse the oratab file and load elements of the Oratab dictionary...
+  if (not f):
+    return {}
+  else:
+    for line in f.readlines():
+      line = line.split('#', 1)[0].strip()
+      Count = line.count(':')
+      if (Count >= 1):
+        OraFlag = ''
+        if (Count == 1):
+          (OraSid, OraHome) = line.split(':')
+        elif (Count == 2):
+          (OraSid, OraHome, OraFlag) = line.split(':')
+        elif (Count >= 3):
+          OraSid = line.split(':')[0]
+          OraHome = line.split(':')[1]
+          OraFlag = line.split(':')[2]
+        OratabDict[OraSid] = OraHome
+
+  return(OratabDict)
+# ---------------------------------------------------------------------------
+# End LoadOratab()
+# ---------------------------------------------------------------------------
+
